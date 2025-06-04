c33faf9a532a73268f7f1722f494536160cda42d
NamYounDong <nyd6849@gmail.com>
2025-06-04 13:47:41 +0900

    cicd_yml_test

--- src/components/Common/GoogleMaps.jsx
+++ src/components/Common/GoogleMaps.jsx
@@ -20,7 +20,6 @@ const GoogleMaps = () => {
 
     // 지도
     const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-    console.log("apiKey :: "+apiKey)
 
     // 정확한 이유는 모르겠는데 이거 처리 안하니까 지맘대로 처음 세팅한 중앙으로 화면이동함.
     const [center, setCenter] = useState({lat: 37.569227, lng: 126.9777256}); 
